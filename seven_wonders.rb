3677006f86364380c4fd11a7307616a849fd0e49
Sophie Messing <sophie.e.messing@gmail.com>
2020-10-05 15:23:02 -0700

    find location and seven wonders are working.

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,29 +1,47 @@
 require 'httparty'
-require "awesome_print"
+require 'prettyprint'
+require 'awesome_print'
 
-BASE_URL = "THE BASE URL FOR THE API REQUEST"
-LOCATION_IQ_KEY = "YOUR API TOKEN"
+BASE_URL = "https://us1.locationiq.com/v1/search.php"
+KEY = "pk.96ba13c368d49149db9ba6490226fe84"
+
 
 def get_location(search_term)
 
+  location = {}
+
+  response = HTTParty.get(BASE_URL, query: {
+      q: search_term,
+      key: KEY,
+      format: "json"
+    }
+  )
+  raise ArgumentError, "coordinates not found!" if response.code != 200
+
+  location[search_term] = { lat: response.first["lat"], lon: response.first["lon"] }
+
+  return location
+
 end
+
 
 def find_seven_wonders
 
-  seven_wonders = ["Great Pyramid of Giza", "Gardens of Babylon", "Colossus of Rhodes", "Pharos of Alexandria", "Statue of Zeus at Olympia", "Temple of Artemis", "Mausoleum at Halicarnassus"]
-
   seven_wonders_locations = []
+
+  seven_wonders = ["Great Pyramid of Giza",
+                   "Gardens of Babylon",
+                   "Colossus of Rhodes",
+                   "Pharos of Alexandria",
+                   "Statue of Zeus at Olympia",
+                   "Temple of Artemis",
+                   "Mausoleum at Halicarnassus"]
 
   seven_wonders.each do |wonder|
     sleep(0.5)
     seven_wonders_locations << get_location(wonder)
   end
-
   return seven_wonders_locations
 end
 
-
-# Use awesome_print because it can format the output nicely
 ap find_seven_wonders
-# Expecting something like:
-# [{"Great Pyramid of Giza"=>{:lat=>"29.9791264", :lon=>"31.1342383751015"}}, {"Gardens of Babylon"=>{:lat=>"50.8241215", :lon=>"-0.1506162"}}, {"Colossus of Rhodes"=>{:lat=>"36.3397076", :lon=>"28.2003164"}}, {"Pharos of Alexandria"=>{:lat=>"30.94795585", :lon=>"29.5235626430011"}}, {"Statue of Zeus at Olympia"=>{:lat=>"37.6379088", :lon=>"21.6300063"}}, {"Temple of Artemis"=>{:lat=>"32.2818952", :lon=>"35.8908989553238"}}, {"Mausoleum at Halicarnassus"=>{:lat=>"37.03788265", :lon=>"27.4241455276707"}}]
